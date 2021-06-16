f79ffd7e7f162af57d363f99ebc819de637d0493
Maksim Zhashkevych <zhashkevychmaksim@gmail.com>
2021-06-16 09:11:24 +0300

    extended lead model

--- internal/models/lead.go
+++ internal/models/lead.go
@@ -8,11 +8,12 @@ type Lead struct {
 	Email     string `json:"email" binding:"required,email"`
 	Phone     string `json:"phone"`
 	Source    string `json:"source" binding:"required"`
+	Niche     string `json:"niche"`
 }
 
 func (l Lead) Info() string {
-	return fmt.Sprintf("Имя: %s\nФамилия: %s\nEmail: %s\nТелефон: %s\nИсточник: %s\n",
-		l.FirstName, l.LastName, l.Email, l.Phone, l.Source)
+	return fmt.Sprintf("Имя: %s\nФамилия: %s\nEmail: %s\nТелефон: %s\nИсточник: %s\nНиша: %s\n",
+		l.FirstName, l.LastName, l.Email, l.Phone, l.Source, l.Niche)
 }
 
 func (l Lead) CardTitle() string {
